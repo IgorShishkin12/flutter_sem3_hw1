17a608272912a1b8d0e2b701570e68d1c0e2cfd0
IgorShishkin12 <qwertyshishkin@yandex.ru>
2025-04-17 18:30:55 +0300

    fix swipe

--- lib/presentation/pages/home_screen.dart
+++ lib/presentation/pages/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -5,8 +7,65 @@ import '../bloc/cat_bloc.dart';
 import 'cat_detail_screen.dart';
 import 'liked_cats_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  double _dragPosition = 0.0;
+  double _rotationAngle = 0.0;
+  final double _swipeThreshold = 100.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onHorizontalDragUpdate(DragUpdateDetails details) {
+    setState(() {
+      _dragPosition += details.primaryDelta!;
+      _rotationAngle = (_dragPosition / 20) * (pi / 180);
+    });
+  }
+
+  void _onHorizontalDragEnd(DragEndDetails details) {
+    if (_dragPosition.abs() > _swipeThreshold) {
+      if (_dragPosition > 0) {
+        // Swiped right - like
+        context.read<CatBloc>().add(LikeCatEvent(context));
+      } else {
+        // Swiped left - dislike
+        context.read<CatBloc>().add(DislikeCatEvent());
+      }
+    }
+
+    // Reset position
+    _controller.animateTo(
+      0,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
+
+    setState(() {
+      _dragPosition = 0.0;
+      _rotationAngle = 0.0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,6 +116,8 @@ class HomeScreen extends StatelessWidget {
               children: [
                 Expanded(
                   child: GestureDetector(
+                    onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                    onHorizontalDragEnd: _onHorizontalDragEnd,
                     onTap: () {
                       Navigator.push(
                         context,
@@ -66,13 +127,22 @@ class HomeScreen extends StatelessWidget {
                       );
                     },
                     child: Center(
-                      child: CachedNetworkImage(
-                        imageUrl: cat.imageUrl,
-                        placeholder:
-                            (context, url) => const CircularProgressIndicator(),
-                        errorWidget:
-                            (context, url, error) => const Icon(Icons.error),
-                        fit: BoxFit.cover,
+                      child: Transform.translate(
+                        offset: Offset(_dragPosition, 0),
+                        child: Transform.rotate(
+                          angle: _rotationAngle,
+                          alignment: Alignment.center,
+                          child: CachedNetworkImage(
+                            imageUrl: cat.imageUrl,
+                            placeholder:
+                                (context, url) =>
+                                    const CircularProgressIndicator(),
+                            errorWidget:
+                                (context, url, error) =>
+                                    const Icon(Icons.error),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
                     ),
                   ),
